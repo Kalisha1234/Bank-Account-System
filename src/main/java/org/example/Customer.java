f67c9c6e071f2cc931cc346099599a5c721e3894
Kalisha1234 <berniceadime12@gmail.com>
2026-01-04 20:54:47 +0000

    adding customer class

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -1,9 +1,28 @@
 package org.example;
 
+import java.util.UUID;
+
 public class Customer {
-    private int id;
+    private String id;
     private String name;
     private String phoneNumber;
     private String email;
 
+        public Customer(String name, String phoneNumber, String email) {
+            this.id = UUID.randomUUID().toString();
+            this.name = name;
+            this.email = email;
+        }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
